a903f8294d0c981e3d372ee9c7f47e5194ef0737
Erik <westervelderik@gmail.com>
2022-11-26 01:59:55 -0500

    Keypad working!

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "lcd.h"
 #include "gpio.h"
@@ -30,26 +31,73 @@ int main(void)
     volatile uint32_t delay = 1500;
     volatile uint8_t inData;
     volatile uint8_t count = 0;
+    volatile uint8_t buffer[16];
+    volatile uint8_t* password = "1234";
+    volatile uint8_t passCount = 4;
+    volatile bool passFail = false;
+
+    SendStringLCD("Enter password:");
+    SendCommandLCD(CURSOR_HOME_SECOND);
+
 
     while(1)
     {
-
         inData = ScanKeypad();
 
+        //If we pressed a key
         if(inData != 0){
-            if(count == 16){
-                SendCommandLCD(CURSOR_HOME_SECOND);
-            }else if(count >= 32){
-                SendCommandLCD(CLEAR_DISPLAY);
-                SendCommandLCD(CURSOR_HOME_FIRST);
-                count = 0;
+            //If we pressed key other then 'Enter'
+            if(inData != '#'){
+                //If we haven't filled the line
+                if(count < 16){
+                    SendCharLCD(inData);
+                    buffer[count] = inData;
+                    count++;
+                }
+
+             //If 'Enter' was pressed
+            }else{
+
+                //If same length of password
+                if(count == passCount){
+                    passFail = true;
+                    for(int x = 0; x < count; x++){
+                        if(buffer[x] != password[x]){
+                            passFail = false;
+                        }
+                    }
+                   if(passFail == true){
+                       break;
+                   }
+                }
+
+                //Not same length or did not match password start over
+
+                    SendCommandLCD(CLEAR_DISPLAY);
+                    SendStringLCD("Incorrect!");
+                    delayMs(2000);
+                    SendCommandLCD(CLEAR_DISPLAY);
+                    SendStringLCD("Enter password:");
+                    SendCommandLCD(CURSOR_HOME_SECOND);
+                    count = 0;
+                }
             }
-            SendCharLCD(inData);
-            inData = 0;
-            count++;
         }
 
+        SendCommandLCD(CLEAR_DISPLAY);
+        SendStringLCD("Correct!  ");
 
+        SendCharLCD(3);
+        SendCharLCD(3);
+        SendCharLCD(3);
+
+
+
+
+
+        while(1){
+
+        }
         /*
         SendCommandLCD(CLEAR_DISPLAY);
         SendCharLCD(count + '0');
@@ -60,7 +108,7 @@ int main(void)
             count = 0;
         }
          */
-    }
+
 }
 
 void SystemInit(void){
